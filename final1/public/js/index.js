d0a5faf1b34046a5e74a8a37106fa5bd7bb68199
Jeehaw Buckadilly <jeehawbuckadilly@gmail.com>
2024-10-24 17:03:41 -0700

    initial commit

---
+++ final1/public/js/index.js
@@ -0,0 +1,343 @@
+// Making Connection
+io.connect("http://localhost:3000");
+
+
+var canvas = document.getElementById("myCanvas");
+var canvas2 = document.getElementById("myCanvas2");
+
+var ctx = canvas.getContext("2d");
+var ctx2 = canvas2.getContext("2d");
+
+// Hero image
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function () {
+	heroReady = true;
+};
+heroImage.src = "images/hero.png";
+
+var tilesReady = false;
+var worldTilesColor = new Image();
+worldTilesColor.onload = function () {
+    tilesReady = true;
+}
+worldTilesColor.src = "images/tileset_1bitGreen.png";
+
+// VIEWPORT
+var viewport = {
+	screen		 : [0,0],
+	startTile	 : [0,0],
+	endTile		 : [0,0],
+	offset		 : [0,0],
+	update		 : function(px, py)
+	{
+		this.offset[0] = Math.floor((this.screen[0]/2) - px);
+		this.offset[1] = Math.floor((this.screen[1]/2) - py);
+
+		var tile = [ Math.floor(px/tileW), Math.floor(py/tileH) ];
+
+		this.startTile[0] = tile[0] - 1 - Math.ceil((this.screen[0]/2) / tileW);
+		this.startTile[1] = tile[1] - 1 - Math.ceil((this.screen[1]/2) / tileH);
+
+		if(this.startTile[0] < 0) { this.startTile[0] = 0; }
+		if(this.startTile[1] < 0) { this.startTile[1] = 0; }
+
+		this.endTile[0] = tile[0] + 1 + Math.ceil((this.screen[0]/2) / tileW);
+		this.endTile[1] = tile[1] + 1 + Math.ceil((this.screen[1]/2) / tileH);
+
+		if(this.endTile[0] >= mapW) { this.endTile[0] = mapW; }
+		if(this.endTile[1] >= mapH) { this.endTile[1] = mapH; }
+
+	}
+};
+
+window.onload = function(){
+
+    
+	viewport.screen = [document.getElementById('myCanvas').width,
+		document.getElementById('myCanvas').height];
+
+    ;
+}
+
+
+
+//socket.on('state', function(players) {
+
+    /*
+    var playerList = [];
+    
+
+    for (var id in players) {
+        var player = players[id];
+        playerList.push(player);
+                   
+        }
+        */
+    
+
+    socket.on('position', function(data) {
+        var currentPlayer = data.currentPlayer;
+        var playerbase = data.playerbase;
+        var map = data.map;
+
+        var playerList = [];
+    
+
+        for (var id in playerbase) {
+            var player = playerbase[id];
+            playerList.push(player);                  
+        }
+
+        viewport.update(currentPlayer.x, currentPlayer.y);
+
+        
+        
+
+        
+        ctx.clearRect(0, 0, 640, 640);
+        
+        
+        ctx.drawImage(heroImage, viewport.offset[0] + currentPlayer.x, viewport.offset[1] + currentPlayer.y, 16, 16);
+
+        
+
+        
+        if (playerList.length == 2){
+
+            console.log("wokdr");
+
+            if( currentPlayer.regionX == playerList[1].regionX  && currentPlayer.regionY == playerList[1].regionY ||
+
+                currentPlayer.regionX == playerList[1].regionX  && currentPlayer.regionY == playerList[1].regionY - 1 ||
+                currentPlayer.regionX == playerList[1].regionX  && currentPlayer.regionY == playerList[1].regionY + 1 ||
+                currentPlayer.regionX == playerList[1].regionX - 1  && currentPlayer.regionY == playerList[1].regionY ||
+                currentPlayer.regionX == playerList[1].regionX + 1  && currentPlayer.regionY == playerList[1].regionY ||
+                currentPlayer.regionX == playerList[1].regionX - 1  && currentPlayer.regionY == playerList[1].regionY - 1 ||
+                currentPlayer.regionX == playerList[1].regionX + 1  && currentPlayer.regionY == playerList[1].regionY - 1 ||
+                currentPlayer.regionX == playerList[1].regionX + 1  && currentPlayer.regionY == playerList[1].regionY + 1 ||
+                currentPlayer.regionX == playerList[1].regionX - 1  && currentPlayer.regionY == playerList[1].regionY + 1
+
+            ){
+
+                var xDifference1 = currentPlayer.regionX - playerList[1].regionX;
+                var yDifference1 = currentPlayer.regionY - playerList[1].regionY;
+
+                var xDifference2 = currentPlayer.regionX - playerList[0].regionX;
+                var yDifference2 = currentPlayer.regionY - playerList[0].regionY;
+
+                ctx.drawImage(heroImage, viewport.offset[0] + playerList[0].x - (320 * xDifference2), viewport.offset[1] + playerList[0].y - (320 * yDifference2), 16, 16);
+
+                ctx.drawImage(heroImage, viewport.offset[0] + playerList[1].x - (320 * xDifference1), viewport.offset[1] + playerList[1].y - (320 * yDifference1), 16, 16);
+
+                //ctx.clearRect(0, 0, 640, 320);
+
+            }
+
+            
+
+        
+            
+    
+    
+    
+        }
+        
+            
+
+        
+
+
+
+        //socket.on('secret', function(matrix) {
+
+            console.log("working xd");
+
+            ctx2.clearRect(0, 0, 640, 640);
+
+
+            if (tilesReady){
+
+            for ( k = 0; k <= 19; k++){
+                for ( l = 0; l <= 19; l++){
+        
+        
+        
+                    //for ( i=0; i < playerList.length; i++){
+                        
+                        ctx2.drawImage(worldTilesColor, ((map[currentPlayer.regionX][currentPlayer.regionY][k][l]%8) * 16), (Math.floor(map[currentPlayer.regionX][currentPlayer.regionY][k][l] / 8) * 16), 16, 16, viewport.offset[0] + k * 16, viewport.offset[1] + l * 16, 16, 16);
+
+                        ctx2.drawImage(worldTilesColor, ((map[currentPlayer.regionX + 1][currentPlayer.regionY][k][l]%8) * 16), (Math.floor(map[currentPlayer.regionX + 1][currentPlayer.regionY][k][l] / 8) * 16), 16, 16, viewport.offset[0] + (k+20) * 16, viewport.offset[1] + l * 16, 16, 16);
+                        ctx2.drawImage(worldTilesColor, ((map[currentPlayer.regionX][currentPlayer.regionY + 1][k][l]%8) * 16), (Math.floor(map[currentPlayer.regionX][currentPlayer.regionY + 1][k][l] / 8) * 16), 16, 16, viewport.offset[0] + k * 16, viewport.offset[1] + (l+20) * 16, 16, 16);
+                        ctx2.drawImage(worldTilesColor, ((map[currentPlayer.regionX - 1][currentPlayer.regionY][k][l]%8) * 16), (Math.floor(map[currentPlayer.regionX - 1][currentPlayer.regionY][k][l] / 8) * 16), 16, 16, viewport.offset[0] + (k-20) * 16, viewport.offset[1] + l * 16, 16, 16);
+                        ctx2.drawImage(worldTilesColor, ((map[currentPlayer.regionX][currentPlayer.regionY - 1][k][l]%8) * 16), (Math.floor(map[currentPlayer.regionX][currentPlayer.regionY - 1][k][l] / 8) * 16), 16, 16, viewport.offset[0] + k * 16, viewport.offset[1] + (l-20) * 16, 16, 16);
+                        ctx2.drawImage(worldTilesColor, ((map[currentPlayer.regionX - 1][currentPlayer.regionY - 1][k][l]%8) * 16), (Math.floor(map[currentPlayer.regionX - 1][currentPlayer.regionY - 1][k][l] / 8) * 16), 16, 16, viewport.offset[0] + (k-20) * 16, viewport.offset[1] + (l-20) * 16, 16, 16);
+                        ctx2.drawImage(worldTilesColor, ((map[currentPlayer.regionX + 1][currentPlayer.regionY - 1][k][l]%8) * 16), (Math.floor(map[currentPlayer.regionX + 1][currentPlayer.regionY - 1][k][l] / 8) * 16), 16, 16, viewport.offset[0] + (k+20) * 16, viewport.offset[1] + (l-20) * 16, 16, 16);
+                        ctx2.drawImage(worldTilesColor, ((map[currentPlayer.regionX - 1][currentPlayer.regionY + 1][k][l]%8) * 16), (Math.floor(map[currentPlayer.regionX - 1][currentPlayer.regionY + 1][k][l] / 8) * 16), 16, 16, viewport.offset[0] + (k-20) * 16, viewport.offset[1] + (l+20) * 16, 16, 16);
+                        ctx2.drawImage(worldTilesColor, ((map[currentPlayer.regionX + 1][currentPlayer.regionY + 1][k][l]%8) * 16), (Math.floor(map[currentPlayer.regionX + 1][currentPlayer.regionY + 1][k][l] / 8) * 16), 16, 16, viewport.offset[0] + (k+20) * 16, viewport.offset[1] + (l+20) * 16, 16, 16);
+                
+                
+                    //}
+        
+        
+                    //if(matrix[i][j][k][l] == 63 || 7 || 6){
+                   // ctx2.drawImage(worldTilesColor, ((map[player.regionX][player.regionY][k][l]%8) * 16), (Math.floor(map[player.regionX][player.regionY][k][l] / 8) * 16), 16, 16, k * 16, l * 16, 16, 16);  
+                   // }	    
+                    
+                }
+        
+            }
+
+
+            }
+
+
+            ctx2.fillStyle = "rgb(250, 25, 250)";
+	        ctx2.font = "18px Helvetica";
+	        ctx2.textAlign = "left";
+	        ctx2.textBaseline = "top";
+	        ctx2.fillText("YOU ROCK " +  currentPlayer.regionX + ":" + currentPlayer.regionY, 32, 32);
+   
+       // });
+
+
+
+
+
+
+
+
+
+
+
+    })
+    
+    
+
+   
+
+    
+    
+    
+        
+
+    
+
+    
+      
+    //});
+    
+
+    
+
+    
+
+    
+    
+
+    
+    
+
+    
+    
+
+    
+
+
+    
+
+
+
+    
+
+
+    
+
+    
+
+    
+
+    
+
+    
+    
+
+    
+
+
+
+    
+
+    
+    
+
+
+
+
+/*
+socket.on('secret', function(secret) {
+    console.log(secret);
+}
+);
+*/
+
+//socket.on('message', function(data) {console.log(data);});
+
+
+
+var movement = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+}
+
+document.addEventListener('keydown', function(event) {switch (event.keyCode) {
+    case 65: 
+    // A
+    movement.left = true;
+    break;
+
+    case 87:
+    // W
+    movement.up = true;
+    break;
+
+    case 68: 
+    // D
+    movement.right = true;
+    break;
+
+    case 83:
+    // S
+    movement.down = true;
+    break;
+
+}});
+    document.addEventListener('keyup', function(event) {switch (event.keyCode) {
+        case 65:
+         // A
+        movement.left = false;
+        break;
+
+        case 87:
+          // W
+        movement.up = false;
+        break;
+
+        case 68:
+           // D
+        movement.right = false;
+        break;
+
+        case 83:
+            // S
+        movement.down = false;
+        break;
+        
+    }});
+    
+socket.emit('new player');
+
+setInterval(function() {socket.emit('movement', movement);}, 1000 / 60);
+
---
+++ final1/server.js
@@ -0,0 +1,277 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+// Set static folder
+app.use(express.static("public"));
+
+// GAME LOGIC
+
+x = [];
+
+for (i=0; i<100; i++){
+	x[i] = [];		
+}
+
+for (i=0; i<100; i++){
+	for (j=0; j<100; j++){					
+		x[i][j] = Math.floor(Math.random() * 100) + 1;				
+	}
+}
+
+matrix = [];
+for (i=0; i<100; i++){
+	matrix[i] = [];
+}
+	function swapLoad(i, j) {
+
+		if (x[i][j] >= 67){	
+			
+			matrix[i][j] = []
+
+			for (k=0; k<=19; k++){
+
+				matrix[i][j][k] = []
+
+				for (l=0; l<=19; l++){ 
+
+					var randomBlock = Math.floor(Math.random() * 1000) + 1;					
+					var density = 60;
+					var grassDensity = 200;
+										
+						if ( randomBlock > density){
+							if ( randomBlock > grassDensity) {
+							matrix[i][j][k][l] = 63;
+						}
+						else{
+							matrix[i][j][k][l] = 6;
+						}
+					}
+					else {
+						matrix[i][j][k][l] = 7;
+					}						
+				}				
+			}	
+		}				
+		else {
+			matrix[i][j] = [];
+			for(k=0; k<=19; k++){
+				matrix[i][j][k] = []
+				for(l=0; l<=19; l++){
+					matrix[i][j][k][l] = 17;
+				}
+			}
+		}	
+}
+
+var players = {
+};
+
+swapLoad(50, 50);
+swapLoad(50, 50 + 1);
+swapLoad(50 + 1, 50);
+swapLoad(50, 50 - 1);
+swapLoad(50 - 1, 50);
+swapLoad(50 - 1, 50 - 1);
+swapLoad(50 + 1, 50 - 1);
+swapLoad(50 - 1, 50 + 1);
+swapLoad(50 + 1, 50 + 1);
+
+
+
+colors = ['red', 'blue', 'green'];
+
+
+
+
+
+
+//console.log(matrix[0][0]);
+
+
+
+
+
+io.on('connection', (socket) => {
+
+  socket.emit('secret', matrix);
+  setTimeout( function() {socket.emit('secret', matrix)}, 200);
+
+  console.log('a user connected:', socket.id);
+  socket.on('disconnect', function() {
+  console.log('user disconnected', socket.id);
+  });
+
+
+  
+
+
+
+
+  socket.on('new player', function() {
+    
+
+    
+
+    players[socket.id] = {
+      x: 60,
+      y: 60,
+      regionX: 50,
+      regionY: 50
+      };
+
+      
+
+      
+
+
+
+      
+
+
+      
+
+    
+
+      
+      
+
+    
+  });
+
+  socket.on('movement', function(data) {
+
+    var player = players[socket.id] || {};
+    if (data.left) {player.x -= 2;}
+    if (data.up) {player.y -= 2;}
+    if (data.right) {player.x += 2;}
+    if (data.down) {player.y += 2;}
+
+    if (player.y < 0)
+      {
+        player.y = 20 * 16;
+        
+        --player.regionY;
+
+
+        if (matrix[player.regionX - 1][player.regionY - 1] === undefined){
+          swapLoad(player.regionX - 1, player.regionY - 1);
+          }
+
+        if (matrix[player.regionX][player.regionY - 1] === undefined){
+          swapLoad(player.regionX, player.regionY - 1);
+          }
+
+        if (matrix[player.regionX + 1][player.regionY - 1] === undefined){
+          swapLoad(player.regionX + 1, player.regionY - 1);
+          }
+        
+      }
+    
+      if (player.x > 20 * 16)
+      {
+
+        
+
+        player.x = 0;
+        
+        ++player.regionX;
+
+        
+        if (matrix[player.regionX + 1][player.regionY + 1] === undefined){
+          swapLoad(player.regionX + 1, player.regionY + 1);
+          }
+
+        if (matrix[player.regionX + 1][player.regionY] === undefined){
+          swapLoad(player.regionX + 1, player.regionY);
+          }
+
+        if (matrix[player.regionX + 1][player.regionY - 1] === undefined){
+          swapLoad(player.regionX + 1, player.regionY - 1);
+          }
+
+        //console.log(matrix[player.regionX][player.regionY]);
+
+        //socket.emit('secret', matrix);
+        
+
+        
+
+        
+        
+        
+        
+      }
+    
+      if (player.y > 20 * 16)
+      {
+        player.y = 0;
+    
+        ++player.regionY;
+
+        if (matrix[player.regionX - 1][player.regionY + 1] === undefined){
+          swapLoad(player.regionX - 1, player.regionY + 1);
+          }
+
+        if (matrix[player.regionX][player.regionY + 1] === undefined){
+          swapLoad(player.regionX, player.regionY + 1);
+          }
+
+        if (matrix[player.regionX + 1][player.regionY + 1] === undefined){
+          swapLoad(player.regionX + 1, player.regionY + 1);
+          }
+        
+        
+      }
+    
+      if (player.x < 0)
+      {
+        player.x = 20 * 16;
+          
+        --player.regionX;
+
+        if (matrix[player.regionX - 1][player.regionY + 1] === undefined){
+          swapLoad(player.regionX - 1, player.regionY + 1);
+          }
+
+        if (matrix[player.regionX - 1][player.regionY] === undefined){
+          swapLoad(player.regionX - 1, player.regionY);
+          }
+
+        if (matrix[player.regionX - 1][player.regionY - 1] === undefined){
+          swapLoad(player.regionX - 1, player.regionY - 1);
+          }
+        
+        
+      }
+
+      socket.emit('position', {playerbase: players, currentPlayer: player, map: matrix});
+      
+
+  });
+
+  
+
+  
+});
+
+//setInterval(function() {io.sockets.emit('message', 'hi!');}, 1000);
+
+
+
+
+/*
+setInterval(function() {
+  io.sockets.emit('state', {players});}, 1000 / 60);
+  */
+  
+
+
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
